ca09755172e34a7aeab4763a370980a86fe11cd7
loganbw <loganbryantweathersby@gmail.com>
2017-10-12 15:04:19 -0400

    added nav bar

--- src/main/java/com/logan/petfinder/controllers/HomeController.java
+++ src/main/java/com/logan/petfinder/controllers/HomeController.java
@@ -1,13 +1,32 @@
 package com.logan.petfinder.controllers;
 
+import com.logan.petfinder.Dao.UserDao;
+import com.logan.petfinder.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 @Controller
 public class HomeController {
+
+    @Autowired
+    UserDao userDao;
+
+
     @RequestMapping(value = "/home" )
-    public String home(){
+    public String home( Principal principal,
+                       Model model){
+        User me = userDao.findByUsername(principal.getName());
+        model.addAttribute("user", me);
         return "home";
     }
+//    public String home(Model model, Principal principal) {
+//        User me = UserDao.findByUsername(principal.getName());
+//        model.addAttribute("user", UserDao.findAll());
+//        return "list";
+//    }
 
 }
