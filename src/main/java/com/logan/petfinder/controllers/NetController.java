388d32637efa1ff3dd5c234a89e1d0c28c9409d9
loganbw <loganbryantweathersby@gmail.com>
2017-10-12 15:14:13 -0400

    created a network controller

--- src/main/java/com/logan/petfinder/controllers/NetController.java
+++ src/main/java/com/logan/petfinder/controllers/NetController.java
@@ -5,26 +5,21 @@ import com.logan.petfinder.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
+
 @Controller
-public class HomeController {
+public class NetController {
 
     @Autowired
     UserDao userDao;
 
-
-    @RequestMapping(value = "/home" )
-    public String home( Principal principal,
+    @RequestMapping(value = "/net")
+    public String net( Principal principal,
                        Model model){
         User me = userDao.findByUsername(principal.getName());
         model.addAttribute("user", me);
-        return "home";
+        return "net";
     }
-
-//
-
 }
